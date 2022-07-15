c44a6a035f26ad266cbf435c8bde2e4f1e401dfa
marty <marty@control-plane.k8s.dingo.services>
2022-07-15 14:20:14 +0000

    updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 cd /projects/$PROJECT
 
+if [ ! -f "/projects/$PROJECT/package.json" ]; then
+    npm init -y
+fi
+
 if [ -f "/projects/$PROJECT/prepare.sh" ]; then
     /bin/bash -c "/projects/$PROJECT/prepare.sh"
 fi
