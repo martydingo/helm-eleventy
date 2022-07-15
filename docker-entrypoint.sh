86e6fa9b64ac4e2089978986679c6463e96eedac
marty <marty@control-plane.k8s.dingo.services>
2022-07-15 13:46:49 +0000

    updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /projects
 if [ -f "/projects/$PROJECT/prepare.sh" ]; then
-    /usr/bin/bash -c "/projects/$PROJECT/prepare.sh"
+    /bin/bash -c "/projects/$PROJECT/prepare.sh"
 fi
 
 npx @11ty/eleventy --input /projects/$PROJECT --output /www --serve
